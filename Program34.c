fa0e73ecca7cbf8912d85874773ddf22ec81470b
Prasad <125743357+Prasadkadam03@users.noreply.github.com>
2023-10-09 20:53:27 +0530

    for loop program

---
+++ Program34.c
@@ -0,0 +1,36 @@
+// Problem Statement :- Take input from user and Print "Hello" for input  times
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+   int iCnt = 0;
+
+    for ( iCnt = 1; iCnt <= iNo; iCnt++ )
+    {
+        
+        printf("Hello %d\n",iCnt); 
+
+    }
+}
+
+int main()
+{
+
+    int iValue = 0;
+
+    printf("Enter the Number of times you want display hello on screen :\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+
+}
+
+
+
+
+
+
+
