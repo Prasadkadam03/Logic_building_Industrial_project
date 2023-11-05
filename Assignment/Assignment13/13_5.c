a1a72000be4c00caca4e07ac0a3376db66a3703a
Prasad <125743357+Prasadkadam03@users.noreply.github.com>
2023-11-06 00:41:14 +0530

    Assignment 13

---
+++ Assignment/Assignment13/13_5.c
@@ -0,0 +1,51 @@
+// Problem statement : Accept N numbers form users and Display elements which are Multiple of 11.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void Display(int Arr[],int iLength)
+{
+    int iCnt = 0;
+
+    printf("Numbers Multiple of 11 are : \n");
+    for(iCnt = 0; iCnt < iLength;iCnt++)
+    {
+        if((Arr[iCnt] % 11 ) == 0 ) 
+        {
+            printf("%d \t",Arr[iCnt]);
+        }
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iSize = 0, iRet = 0,iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter the number of elements :\n");
+    scanf("%d",&iSize);
+
+    p = (int *)malloc( iSize * sizeof(int));
+
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory \n" );
+        return -1;
+    }
+
+    printf(" Enter %d elements : \n",iSize);
+
+    for(iCnt = 0; iCnt < iSize ; iCnt++)
+    {
+        printf("Enter the element : %d \n",iCnt + 1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    Display(p , iSize);
+
+    return 0;
+    
+}
