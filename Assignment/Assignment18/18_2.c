0fa51e26426e163e2ce135c16c38b31fa51d8f0c
Prasad <125743357+Prasadkadam03@users.noreply.github.com>
2023-11-15 13:52:32 +0530

    Assignment 18

---
+++ Assignment/Assignment18/18_2.c
@@ -0,0 +1,39 @@
+// Problem Statement : Accept number form user and display below pattern
+/* 
+Enter numner of rows and columns :
+5
+5
+1       2       3       4       5
+1       2       3       4       5
+1       2       3       4       5
+1       2       3       4       5
+1       2       3       4       5
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{   
+    int i = 0, j = 0;
+    
+    for(i = 1 ; i <= iRow ; i++)
+    {
+        for(j = 1 ; j <= iCol ; j++)
+        {
+            printf("%d\t" , j);
+        } 
+        printf("\n");
+    } 
+}
+
+int main()
+{
+    int iValue1 = 0 , iValue2 = 0;
+
+    printf("Enter numner of rows and columns : \n");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;      
+}
