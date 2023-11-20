7e934e30f5087358a7865ad859b37912220063d1
Prasad <125743357+Prasadkadam03@users.noreply.github.com>
2023-11-20 22:56:08 +0530

    Singly Linked List

---
+++ Program133.c
@@ -0,0 +1,20 @@
+// Started with linked list
+
+#include<stdio.h>
+#include<stdlib.h>
+
+// Structure Declaration
+struct node
+{
+    int data;           // 4 bytes
+    struct node *next;  // 8 bytes
+};
+
+int main()
+{
+    struct node *First = NULL;
+
+
+    
+    return 0;
+}
