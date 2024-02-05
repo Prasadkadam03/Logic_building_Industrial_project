33bbff1b4c2f569220d38bbcf5f19d07a541b3d4
Prasadkadam03 <prasadkadam29503@gmail.com>
2024-02-05 10:04:53 +0530

    Bitwise opertaors

---
+++ Program177.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Program177
+{
+    public static boolean ChkBit(int iNo)
+    {
+        int iMask = 256;
+        int iResult = 0;
+        iResult = iNo & iMask;
+
+        if (iResult == iMask)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iNo = 0;
+        boolean bRet = false; 
+
+        System.out.println("Enter number : ");
+        iNo = sobj.nextInt();
+
+        bRet = ChkBit(iNo);
+
+        if (bRet == true)
+        {
+            System.out.println("9th bit is ON");
+        }
+        else
+        {
+            System.out.println("9th bit is OFF");
+        }
+
+    }
+}
+
+
---
+++ Program178.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Program178
+{
+    public static boolean ChkBit(int iNo)
+    {
+        int iMask = 265536;
+        int iResult = 0;
+        iResult = iNo & iMask;
+
+        if (iResult == iMask)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iNo = 0;
+        boolean bRet = false; 
+
+        System.out.println("Enter number : ");
+        iNo = sobj.nextInt();
+
+        bRet = ChkBit(iNo);
+
+        if (bRet == true)
+        {
+            System.out.println("17th bit is ON");
+        }
+        else
+        {
+            System.out.println("17th bit is OFF");
+        }
+
+    }
+}
+
+
